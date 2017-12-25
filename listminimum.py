81e12133eb20168101088d61524b7ec42ed8bd12
taahahassan <30376976+taahahassan@users.noreply.github.com>
2017-12-24 23:23:18 -0500

    Create listminimum.py

---
+++ listminimum.py
@@ -0,0 +1,13 @@
+size = int(input()) #number of integers that need to be sorted
+a = []
+
+for i in range(0, size):
+  i = int(input()) #type numbers that need to be sorted
+  a.append(i)
+
+a = sorted(a)
+
+for i in range(0,size):
+  print (a[i])
+  
+  
