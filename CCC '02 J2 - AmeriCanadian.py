7c577f56be7b52421f99be9a9f350d02b2c04bdd
taahahassan <30376976+taahahassan@users.noreply.github.com>
2018-01-16 12:04:59 -0500

    Update CCC '02 J2 - AmeriCanadian.py

--- CCC '02 J2 - AmeriCanadian.py
+++ CCC '02 J2 - AmeriCanadian.py
@@ -1,9 +1,14 @@
 N = input().split(',')
 
 for word in N:
-  if "or" in word and len(word) > 4:
-    words = word.replace("or","our")
-  if word == "quit!":
-    raise SystemExit
-
+  if word != "quit!":
+    print (word)
+    x = len(word)
+    if "or" in word: 
+      if x > 4:
+        words = word.replace("or","our")
+  elif word == "quit!":
+    quit()
+  
+print (N)
 print (words)
